3f8cb3ed3062650cf699a4418e1630a0c5b2f0d7
feroliva02 <olivafern2@gmail.com>
2023-09-26 16:39:02 -0300

    Instalacion

--- moru-app/src/App.jsx
+++ moru-app/src/App.jsx
@@ -1,8 +1,18 @@
+import Home from "./views/Home"
+import Acceso from "./views/Acceso"
+import { Route, Routes, useLocation } from "react-router-dom"
+
 function App() {
+  const {pathname} = useLocation();
+
   return (
-    <>
+    <div>
       <h1>Moru App</h1>
-    </>
+      <Routes>
+        
+      </Routes>
+    </div>
+
   )
 }
 
