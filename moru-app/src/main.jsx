a4ae1f2bd90ba3f38cd69917c31d1837d8079a8e
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-09-26 17:52:00 -0300

    Store listísimo

--- moru-app/src/main.jsx
+++ moru-app/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./redux/store.js"
 
-ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  document.getElementById('root')
-);
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
