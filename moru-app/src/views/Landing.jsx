efd235a496e60c517ee94cdb8bf4b5b49f1302d9
felipefraga <felifraga10@gmail.com>
2023-09-27 09:50:58 -0300

    Landing terminada

--- moru-app/src/views/Landing.jsx
+++ moru-app/src/views/Landing.jsx
@@ -1,12 +1,21 @@
+import image from '../images/image.png';
+import { Link } from 'react-router-dom'
+
 const Landing = () => {
-
-
     return (
-        <div >
-            <h1 className="text-4xl uppercase">Soy el Landing</h1>
-            <button>Hola</button>
+        <div className="fixed inset-0 overflow-hidden bg-cover bg-center h-screen" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover' }}>
+            <div className="h-screen flex flex-col justify-start items-center">
+                <div className="mt-auto flex flex-col items-center">
+                    <Link to='./Login'>
+                        <button className="mb-2 text-purple-900 w-48 h-12 bg-white rounded-lg ">Cuenta Personal</button>
+                    </Link>
+                    <Link to='./Login'>
+                        <button className="mb-16 text-purple-900 w-48 h-12 bg-white rounded-lg ">Cuenta Empresarial</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Landing
+export default Landing;
