2a945af8241756f28ad51382b9c4b7c4d11bdab0
feroliva02 <olivafern2@gmail.com>
2023-09-28 13:13:29 -0300

    Links to

--- moru-app/src/App.jsx
+++ moru-app/src/App.jsx
@@ -1,10 +1,10 @@
 import Home from "./views/Home"
-//import Landing from "./views/Landing"
+import Landing from "./views/Landing"
 import Login from "./views/Login.jsx"
 import "./index.css"
 import RegisterUser from "./views/RegisterUser.jsx"
 import RegisterShop from "./views/RegisterShop.jsx"
-import Nav from "./components/nav"
+import Nav from "./components/Nav"
 import ShoppingCart from "./views/ShoppingCart"
 import { Route, Routes, useLocation } from "react-router-dom"
 
@@ -25,7 +25,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/registeruser" element={<RegisterUser />}></Route>
         <Route path="/registershop" element={<RegisterShop />}></Route>
-        {/* <Route path="/landing" element={<Landing />}></Route> */}
+        <Route path="/landing" element={<Landing />}></Route>
         <Route path="/carrito-de-compras" element={<ShoppingCart/>}/>
       </Routes>
     </div>
--- moru-app/src/components/Nav.jsx
+++ moru-app/src/components/Nav.jsx
@@ -46,11 +46,11 @@ const Nav = () => {
           </button>
 
           <li className="flex flex-col space-y-10 text-xl">
-            <ul className="flex justify-center space-x-4"> <img className="w-7" src={storeIcon} alt="store" /><Link>Tienda</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={favIcon} alt="fav" /><Link>Favoritos</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={publishIcon} alt="publish" /><Link>Publicar</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={countIcon} alt="count" /><Link>Cuenta</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={supportIcon} alt="publish" /><Link>Soporte</Link></ul>
+            <ul className="flex justify-center space-x-4"> <img className="w-7" src={storeIcon} alt="store" /><Link to= "/">Tienda</Link></ul>
+            <ul className="flex justify-center space-x-4" ><img className="w-7" src={favIcon} alt="fav" /><Link to="/fav">Favoritos</Link></ul>
+            <ul className="flex justify-center space-x-4" ><img className="w-7" src={publishIcon} alt="publish" /><Link to="/publish">Publicar</Link></ul>
+            <ul className="flex justify-center space-x-4" ><img className="w-7" src={countIcon} alt="count" /><Link to="/myaccount">Cuenta</Link></ul>
+            <ul className="flex justify-center space-x-4" ><img className="w-7" src={supportIcon} alt="publish" /><Link to="/support">Soporte</Link></ul>
           </li>
         </div>
       </div>
