9b089325020cb8dc5368e384ea5c838eece3efa7
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-09-28 15:21:35 -0300

    tienda y producto comentados en nav

--- moru-app/src/components/Nav.jsx
+++ moru-app/src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+//TODO usar iconos de react
 import shoppingIcon from "../images/icons/carrito-de-compras.png"
 import sandwichIcon from "../images/icons/sandwich-icon.jpg"
 import storeIcon from '../images/icons/store.svg'
@@ -19,23 +20,22 @@ const Nav = () => {
 
   return (
     <nav className="flex flex-col sticky top-0  bg-white  z-50">
-      <div className="flex justify-between w-full items-center px-6 py-2 shadow-lg rounded-bl-lg rounded-br-lg ">
-      <button onClick={() => {setOpenMenu(true)}}>
-        <img className="w-7" src={sandwichIcon} alt="sandwichIcon" />
-          </button>
+      <div className="flex justify-between w-full items-center px-6 py-2 shadow-lg rounded-bl-lg rounded-br-xl ">
+        <button onClick={() => {setOpenMenu(true)}}>
+          <img className="w-7" src={sandwichIcon} alt="sandwichIcon" />
+        </button>
 
-            {
-                isAuthenticated ? 
-                <LogOutButton /> 
-                : <button onClick={() => loginWithRedirect()}>Ingresar</button>
-            }
+        <div className="flex items-center space-x-4">
+          {
+            isAuthenticated 
+            ? <LogOutButton /> 
+            : <button onClick={() => loginWithRedirect()}>Ingresar</button>
+          }
 
-        {/* <Link to="/"><img className="w-20" src={logoMoru} alt="Moru App" /></Link> */}
-
-        <Link to="/carrito-de-compras">
-          <img className="w-12" src={shoppingIcon} alt="shoppingIcon" />
-        </Link>
+          <Link to="/carrito-de-compras"><img className="w-12" src={shoppingIcon} alt="shoppingIcon" /></Link>
       </div>
+
+        </div>
       <div onClick={() => {setOpenMenu(false)}} className={`${!openMenu && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed backdrop-blur-sm`}></div>
 
       <div className={`${openMenu ? 'w-72' : 'w-0' } bg-white min-h-screen  fixed top-0 left-0 right-0 transition-all duration-300`}>
@@ -46,10 +46,10 @@ const Nav = () => {
           </button>
 
           <li className="flex flex-col space-y-10 text-xl">
-            <ul className="flex justify-center space-x-4"> <img className="w-7" src={storeIcon} alt="store" /><Link>Tienda</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={favIcon} alt="fav" /><Link>Favoritos</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={publishIcon} alt="publish" /><Link>Publicar</Link></ul>
-            <ul className="flex justify-center space-x-4" ><img className="w-7" src={countIcon} alt="count" /><Link>Cuenta</Link></ul>
+{/*             <ul className="flex justify-center space-x-4"> <img className="w-7" src={storeIcon} alt="store" /><Link>Tienda</Link></ul>
+ */}        <ul className="flex justify-center space-x-4" ><img className="w-7" src={favIcon} alt="fav" /><Link>Favoritos</Link></ul>
+{/*             <ul className="flex justify-center space-x-4" ><img className="w-7" src={publishIcon} alt="publish" /><Link>Publicar</Link></ul>
+ */}        <ul className="flex justify-center space-x-4" ><img className="w-7" src={countIcon} alt="count" /><Link>Cuenta</Link></ul>
             <ul className="flex justify-center space-x-4" ><img className="w-7" src={supportIcon} alt="publish" /><Link>Soporte</Link></ul>
           </li>
         </div>
