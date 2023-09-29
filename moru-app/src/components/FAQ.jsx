d40ee14fb0f2b19b4e8c38162e13a0feca04f907
feroliva02 <olivafern2@gmail.com>
2023-09-29 01:31:05 -0300

    Preguntas

--- moru-app/src/components/FAQ.jsx
+++ moru-app/src/components/FAQ.jsx
@@ -1,23 +1,25 @@
 const FAQ = () => {
     return (
         <div>
-            <h1 className="text-decoration: underline text-center text-xl italic">PREGUNTAS FRECUENTES</h1>
-            <div className="mt-4">
-                <h1 className="italic bg-gray-100">Pregunta 1</h1>
-                <h1>Respuesta 1</h1>
+            <h1 className="text-decoration: underline text-center text-xl italic mt-2 mb-7">PREGUNTAS FRECUENTES</h1>
+            <div className="ml-5 mr-5">
+            <div className="mt-4 mb-8">
+                <h1 className="italic bg-gray-100">¿Quiénes somos?</h1>
+                <h1>Mōru App nace en 2019 de la necesidad de simplificar la búsqueda, compra y comparación de productos.</h1>
             </div>
-            <div className="mt-4">
-                <h1 className="italic bg-gray-100">Pregunta 2</h1>
-                <h1>Respuesta 2</h1>
+            <div className="mt-4 mb-8">
+                <h1 className="italic bg-gray-100">¿Cuál es el significado del nombre Mōru App?</h1>
+                <h1>El nombre tiene un significado fuerte y relevante que conecta la idea de un centro comercial con el concepto digital de una app.</h1>
             </div>
-            <div className="mt-4">
-                <h1 className="italic bg-gray-100">Pregunta 3</h1>
-                <h1>Respuesta 3</h1>
+            <div className="mt-4 mb-8">
+                <h1 className="italic bg-gray-100">¿Cuál es su eslogan?</h1>
+                <h1>“Comercio local, ahora online”</h1>
             </div>
-            <div className="mt-4">
-                <h1 className="italic bg-gray-100">Pregunta 4</h1>
-                <h1>Respuesta 4</h1>
+            <div className="mt-4 mb-8">
+                <h1 className="italic bg-gray-100">¿Cuál es nuestro objetivo?</h1>
+                <h1>Mōru App va enfocado a dinamizar la economía local y democratizar el comercio electrónico en todos los municipios de América Latina</h1>
             </div>
+        </div>
         </div>
     )
 }
