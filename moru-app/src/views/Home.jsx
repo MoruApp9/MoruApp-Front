21e2f8e1b59214fa9984b61a09f55b80d1263c5b
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-10-02 01:36:51 -0300

    Nav estilos refactorized

--- moru-app/src/views/Home.jsx
+++ moru-app/src/views/Home.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import Advertising from "../components/Advertising"
 import AllProducts from "../components/AllProducts"
 import Filters from "../components/Filters"
-import Categories from '../components/Categories';
+import Categories from "../components/Categories"
 
 const Home = () => {
   const productsFiltered = useSelector((state) => state.productsFiltered)
@@ -11,10 +11,10 @@ const Home = () => {
   return (
     <div>
       {!productsFiltered.length && <Advertising />}
-      {!productsFiltered.length && <Categories/>}
+      {!productsFiltered.length && <Categories />}
       {!productsFiltered.length && <AllProducts />}
       {productsFiltered.length && <Filters />}
-    </div>
+      </div>
   )
 }
 
