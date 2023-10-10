6a82cba0c76b79d61b712b445c12fe3af7645083
felipefraga <felifraga10@gmail.com>
2023-10-09 21:14:04 -0300

    arregle las sucursales y logout

--- moru-app/src/views/PostProduct.jsx
+++ moru-app/src/views/PostProduct.jsx
@@ -13,6 +13,10 @@ const PostProduct = () => {
   const location = useLocation();
   const id = location.search.slice(1)
   const [imageUpload, setImageUpload] = useState("")
+  const [specificCategories, setSpecificCategories] = useState([]);
+  const dispatch = useDispatch();
+  const dataUser = GetLocalStorage(); 
+  const navigate = useNavigate()
   
   const handleOnChange = async (event) => {
     const imagen = await uploadImageClaudinary(event)
@@ -21,10 +25,6 @@ const PostProduct = () => {
     //console.log(await uploadImageClaudinary(event)); //url creada mostrada en consola
   }
 
-  const [specificCategories, setSpecificCategories] = useState([]);
-  const dispatch = useDispatch();
-  const dataUser = GetLocalStorage(); 
-  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {  //hace la funcion asincrona para poder esperar a que se resuelva la promesa de Categorias
@@ -97,9 +97,9 @@ const PostProduct = () => {
 
           }}
 
-          onSubmit={(valores) => {
+          onSubmit={async (valores) => {
             valores.image = imageUpload
-            postProduct(valores);
+            await postProduct(valores);
             navigate('/');
           }}
         >
