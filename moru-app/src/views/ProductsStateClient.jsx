d9e78483cbcdc477ba8f125525626e35630613c2
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-10-14 14:20:48 -0300

    Navegación en Pendiente view Mejorada

--- moru-app/src/views/ProductsStateClient.jsx
+++ moru-app/src/views/ProductsStateClient.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getHistoryOfOrderedProducts } from "../services/services"
 import { GetLocalStorage } from "../localStorage/GetLocalStorage"
 import { useDispatch, useSelector } from "react-redux"
 import Product from "../components/Product"
 import { setProductsOrderedToStore } from "../redux/productsOrderedSlice"
-import { cleanProductsOrderedFilteredFromStore, setProductsOrderedFilteredToStore } from "../redux/productsOrderedFilteredSlice"
+import {
+  cleanProductsOrderedFilteredFromStore,
+  setProductsOrderedFilteredToStore,
+} from "../redux/productsOrderedFilteredSlice"
 
 const ProductsStateClient = () => {
   const currentUser = GetLocalStorage()
@@ -12,6 +15,8 @@ const ProductsStateClient = () => {
   const productsOrderedFilteredFromStore = useSelector(
     (state) => state.productsOrderedFiltered
   )
+  const [selectedState, setSelectedState] = useState("Todos")
+
   const dispatch = useDispatch()
 
   const updateStore = async () => {
@@ -27,7 +32,8 @@ const ProductsStateClient = () => {
   const handleTodosButton = async (event) => {
     event.stopPropagation()
     event.preventDefault()
-    
+
+    setSelectedState("Todos")
     dispatch(cleanProductsOrderedFilteredFromStore())
   }
 
@@ -39,7 +45,8 @@ const ProductsStateClient = () => {
       (product) => product.status === "pending"
     )
     pendingsProducts.length
-      ? dispatch(setProductsOrderedFilteredToStore(pendingsProducts))
+      ? (dispatch(setProductsOrderedFilteredToStore(pendingsProducts)),
+        setSelectedState("Pendiente"))
       : window.alert("No hay productos pendientes")
   }
 
@@ -47,9 +54,12 @@ const ProductsStateClient = () => {
     event.stopPropagation()
     event.preventDefault()
 
-    const sentProducts = productsOrderedFromStore.filter(product => product.status === "send")
+    const sentProducts = productsOrderedFromStore.filter(
+      (product) => product.status === "send"
+    )
     sentProducts.length
-      ? dispatch(setProductsOrderedFilteredToStore(sentProducts))
+      ? (dispatch(setProductsOrderedFilteredToStore(sentProducts)),
+        setSelectedState("Enviado"))
       : window.alert("No hay productos enviados")
   }
 
@@ -57,23 +67,44 @@ const ProductsStateClient = () => {
     event.stopPropagation()
     event.preventDefault()
 
-    const finishedProducts = productsOrderedFromStore.filter(product => product.status === "finish")
+    const finishedProducts = productsOrderedFromStore.filter(
+      (product) => product.status === "finish"
+    )
     finishedProducts.length
-      ? dispatch(setProductsOrderedFilteredToStore(finishedProducts))
+      ? (dispatch(setProductsOrderedFilteredToStore(finishedProducts)),
+        setSelectedState("Finalizado"))
       : window.alert("No hay productos finalizados")
   }
 
   return (
     <section className="flex flex-col ">
       <div className=" bg-white flex justify-center mx-auto w-fit my-6 space-x-4 p-4 font-roboto-slab border rounded-full sticky top-24 ">
-        <button onClick={handleTodosButton} className="border-r font-bold pr-2">Todos</button>
-        <button onClick={handlePendienteButton} className="border-r pr-2">
+        <button
+          onClick={handleTodosButton}
+          className={`border-r pr-2 ${
+            selectedState === "Todos" && "font-bold"
+          }`}>
+          Todos
+        </button>
+        <button
+          onClick={handlePendienteButton}
+          className={`border-r pr-2 ${
+            selectedState === "Pendiente" && "font-bold"
+          }`}>
           Pendiente
         </button>
-        <button onClick={handleEnviadoButton} className="border-r pr-2">
+        <button
+          onClick={handleEnviadoButton}
+          className={`border-r pr-2 ${
+            selectedState === "Enviado" && "font-bold"
+          }`}>
           Enviado
         </button>
-        <button onClick={handleFinalizadoButton}>Finalizado</button>
+        <button
+          onClick={handleFinalizadoButton}
+          className={`${selectedState === "Finalizado" && "font-bold"}`}>
+          Finalizado
+        </button>
       </div>
 
       {productsOrderedFilteredFromStore.length > 0 ? (
